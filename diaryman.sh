d0513017d8e2fb29b0af12c61e264e25b58db4dd
Rocky Li <aperocky@gmail.com>
2023-01-15 20:15:20 -0500

    Validate  arg and allow cat of current diary.

--- diaryman.sh
+++ diaryman.sh
@@ -3,13 +3,34 @@
 # Set your DIARY_DIR in bashrc. else it goes into home folder.
 cd $DIARY_DIR;
 
-# Get current date or date you want to make up for
 if [[ ! -z "$1" ]]; then
+    # cat the current days' diary in terminal for copying
+    if [[ "$1" == "cat" ]]; then
+        year=$(date +%Y);
+        month=$(date +%m);
+        datename=$(date +%F);
+        cd $year;
+        cd $month;
+        cat "$datename.md";
+        if [ "$?" -ne "0" ]; then
+            echo "Create the diary first before cat";
+            exit 1;
+        fi
+        exit 0;
+    fi
+    # Allowing retroactive diary entries with $ diary Y-m-d
+    # Validate date format
+    date -jf %Y-%m-%d $1 > /dev/null 2>&1;
+    if [ "$?" -ne "0" ]; then
+        echo "Date is not formatted correctly";
+        exit 1;
+    fi
     year=$(date -jf %Y-%m-%d $1 +%Y);
     month=$(date -jf %Y-%m-%d $1 +%m);
-    datename=$1
+    datename=$1;
     weekday=$(date -jf %Y-%m-%d $1 +%A);
 else
+    # By default, if there are no argument, it creates/open the current days' file.
     year=$(date +%Y);
     month=$(date +%m);
     datename=$(date +%F);
--- readme.md
+++ readme.md
@@ -4,4 +4,6 @@ Diary Manager for lazy software engineers who use vim.
 
 `diaryman` automatically creates a file in specified diary directory if it doesn't exist: `$DIARY_DIR/$year/$month/$day.md` and opens it with `vim` in edit mode. If the file already exist, it will just open it, this allow multiple edits throughout the day.
 
+It also allow you to make up for diaries missed by appending a date behind the diary, e.g. `diary 2020-01-01` will open the diary on that date. Additionally, `diary cat` will `cat` the content of current days' diary.
+
 This works great for personal diaries and also for work task organization.
