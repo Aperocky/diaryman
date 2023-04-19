98327bd1cdb28a74db52731e5531cf11c23d6265
cmflynn <cflynn@drivewealth.tech>
2023-04-19 10:24:02 -0400

    adds find command

--- diaryman.sh
+++ diaryman.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Change to the diary directory
 # Set your DIARY_DIR in bashrc. else it goes into home folder.
-cd $DIARY_DIR;
+cd "$DIARY_DIR" || exit;
 
-if [[ ! -z "$1" ]]; then
+if [[ -n "$1" ]]; then
     # cat the current days' diary in terminal for copying
     if [[ "$1" == "cat" ]]; then
         year=$(date +%Y);
         month=$(date +%m);
         datename=$(date +%F);
-        cd $year;
-        cd $month;
+        cd "$year" || exit;
+        cd "$month" || exit;
         cat "$datename.md";
         if [ "$?" -ne "0" ]; then
             echo "Create the diary first before cat";
@@ -18,6 +18,29 @@ if [[ ! -z "$1" ]]; then
         fi
         exit 0;
     fi
+
+    # Find command to search all diaries for a string
+    if [[ "$1" == "find" ]]; then
+        if [[ -z "$2" ]]; then
+            echo "Please provide a search string";
+            exit 1;
+        fi
+
+        if [[ ! -d "$DIARY_DIR" ]]; then
+            echo "Diary directory does not exist";
+            exit 1;
+        fi
+
+        # Use find command to search all diaries for the string
+        # Print the filename and line containing the string
+        find "$DIARY_DIR" \
+            -type f \
+            -name "*.md" \
+            -print0 \
+            | xargs -0 grep -H "$2" --color=always
+        exit 0;
+    fi
+
     # Allowing retroactive diary entries with $ diary Y-m-d
     # Validate date format
     date -jf %Y-%m-%d $1 > /dev/null 2>&1;
@@ -25,10 +48,10 @@ if [[ ! -z "$1" ]]; then
         echo "Date is not formatted correctly";
         exit 1;
     fi
-    year=$(date -jf %Y-%m-%d $1 +%Y);
-    month=$(date -jf %Y-%m-%d $1 +%m);
+    year=$(date -jf %Y-%m-%d "$1" +%Y);
+    month=$(date -jf %Y-%m-%d "$1" +%m);
     datename=$1;
-    weekday=$(date -jf %Y-%m-%d $1 +%A);
+    weekday=$(date -jf %Y-%m-%d "$1" +%A);
 else
     # By default, if there are no argument, it creates/open the current days' file.
     year=$(date +%Y);
@@ -39,17 +62,17 @@ fi
 
 # Create folders if they don't exist.
 if [[ ! -d $year ]]; then
-    mkdir $year;
-    cd $year;
+    mkdir "$year";
+    cd "$year" || exit;
 else
-    cd $year;
+    cd "$year" || exit;
 fi
 
 if [[ ! -d $month ]]; then
-    mkdir $month;
-    cd $month;
+    mkdir "$month" || exit;
+    cd "$month" || exit;
 else
-    cd $month;
+    cd "$month" || exit;
 fi
 
 # Create diary files in the appropriate places and opens them.
--- readme.md
+++ readme.md
@@ -2,8 +2,22 @@
 
 Diary Manager for lazy software engineers who use vim.
 
-`diaryman` automatically creates a file in specified diary directory if it doesn't exist: `$DIARY_DIR/$year/$month/$day.md` and opens it with `vim` in edit mode. If the file already exist, it will just open it, this allow multiple edits throughout the day.
+`diaryman` automatically creates a file in specified diary directory if it doesn't exist: 
+`$DIARY_DIR/$year/$month/$day.md` and opens it with `vim` in edit mode. If the file already exist, 
+it will just open it, this allow multiple edits throughout the day.
 
-It also allow you to make up for diaries missed by appending a date behind the diary, e.g. `diary 2020-01-01` will open the diary on that date. Additionally, `diary cat` will `cat` the content of current days' diary.
+It also allow you to make up for diaries missed by appending a date behind the diary, 
+e.g. `diary 2020-01-01` will open the diary on that date. 
+
+### commands
+`cat` the entire contents of current days' diary.
+```sh
+diary cat
+```
+
+`find`, search all diaries for a string, then return the diary filepath and the matched line for that string. 
+```sh
+diary find jimmyRus
+```
 
 This works great for personal diaries and also for work task organization.
